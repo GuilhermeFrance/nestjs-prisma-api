d1b2b291204a202e9267a073eff0dcadffdf7779
Guilherme <guilhermefrance9@gmail.com>
2025-09-30 16:56:47 -0300

    interceptors

---
+++ src/common/filters/http-exception/http-exception.filter.ts
@@ -0,0 +1,23 @@
+import { ArgumentsHost, Catch, HttpException } from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch()
+export class HttpExceptionFilter<T extends HttpException> {
+  catch(exception: T, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    const status = exception.getStatus();
+    const exceptionResponse = exception.getResponse();
+
+    const error =
+      typeof response === 'string'
+        ? { message: exceptionResponse }
+        : (exceptionResponse as object);
+
+    response.status(status).json({
+      ...error,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { UnauthorizedInterceptor } from './common/interceptors/unauthorized.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,8 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  //app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new UnauthorizedInterceptor());
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
